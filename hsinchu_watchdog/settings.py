dc0eff4b2fb81733746ec822940a2b7e54088290
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-25 00:54:42 +0800

    test

--- hsinchu_watchdog/settings.py
+++ hsinchu_watchdog/settings.py
@@ -79,18 +79,12 @@ WSGI_APPLICATION = 'hsinchu_watchdog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-import dj_database_url
-db_from_env = dj_database_url.config()
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
