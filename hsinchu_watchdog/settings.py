16cb8aa4c6dbad98be23a0422c1f374c5e7eec83
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-11-26 13:53:35 +0800

    society

--- hsinchu_watchdog/settings.py
+++ hsinchu_watchdog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '59)r(_lqj#%dam+q9e!$m=5i33ge$^7od7c%+of2u(33c8+a7!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hsinchu-whatchdog.herokuapp.com']
+ALLOWED_HOSTS = ['hsinchu-whatchdog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
--- roadmap/views.py
+++ roadmap/views.py
@@ -5,7 +5,7 @@ from roadmap.repository import LocationDB
 # import pandas as pd
 
 # Create your views here.
-def index(request, dataset='得獎美食'):
+def index(request, dataset='哺乳室'):
     LDB = LocationDB()
     rows, locations, names = LDB.getLocationsByDataset(dataset)
     template = loader.get_template('roadmap/index.html')
